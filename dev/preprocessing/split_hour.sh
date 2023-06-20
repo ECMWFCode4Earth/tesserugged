f1aa14f51c2e6feaacfa602ba8f4e3f73c2d710b
Konrad Mayer <konrad.mayer@zamg.ac.at>
2023-06-20 07:06:27 +0000

    DEV: reorganize data
    Ref: #2, #3

---
+++ dev/preprocessing/split_hour.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+##################################################################
+#Description    : Rechunk data from montly files to partitions per
+#                 hour of the day
+#Creation Date  : 2023-06-20
+#Author         : Konrad Mayer
+##################################################################
+
+# find project root (by finding git root) to be able to start the script from
+# anywhere within the project, but still avoid absolute paths
+projectroot=$(git rev-parse --show-toplevel)
+
+# split ERA5 data by hour
+cdo splithour -select,name=t2m ${projectroot}/dat/TRAINING/ERA5/t2m_era5_[!lsm]*_025deg.nc ${projectroot}/dat/PREPROCESSED/ERA5/t2m_era5_
+
+# split CERRA data by hour
+cdo splithour -select,name=t2m ${projectroot}/dat/TRAINING/CERRA/t2m_cerra_[!lsm]*_005deg.nc ${projectroot}/dat/PREPROCESSED/CERRA/t2m_cerra_
