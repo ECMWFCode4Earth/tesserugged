375b72f47db963a7c9e007a7b2ba5ada2d177417
Konrad Mayer <konrad.mayer@zamg.ac.at>
2023-07-20 08:19:22 +0000

    DEV: preprocessing for test data

--- dev/preprocessing/split_hour.sh
+++ dev/preprocessing/split_hour.sh
@@ -11,8 +11,17 @@
 # anywhere within the project, but still avoid absolute paths
 projectroot=$(git rev-parse --show-toplevel)
 
+# # TRAINING DATA
+# # split ERA5 data by hour
+# cdo splithour -sellonlatbox,-6.86,5.11,44.96,36.99 -select,name=t2m ${projectroot}/dat/TRAINING/RAW/ERA5/t2m_era5_[!lsm]*_025deg.nc ${projectroot}/dat/TRAINING/PREPROCESSED/ERA5/t2m_era5_
+
+# # split CERRA data by hour
+# cdo splithour -sellonlatbox,-6.86,5.11,44.96,36.99 -select,name=t2m ${projectroot}/dat/TRAINING/RAW/CERRA/t2m_cerra_[!lsm]*_005deg.nc ${projectroot}/dat/TRAINING/PREPROCESSED/CERRA/t2m_cerra_
+
+
+# TEST DATA
 # split ERA5 data by hour
-cdo splithour -sellonlatbox,-6.86,5.11,44.96,36.99 -select,name=t2m ${projectroot}/dat/TRAINING/RAW/ERA5/t2m_era5_[!lsm]*_025deg.nc ${projectroot}/dat/TRAINING/PREPROCESSED/ERA5/t2m_era5_
+cdo splithour -sellonlatbox,-6.86,5.11,44.96,36.99 -select,name=t2m ${projectroot}/dat/TESTING/RAW/ERA5/t2m_era5_[!lsm]*_025deg.nc ${projectroot}/dat/TESTING/PREPROCESSED/ERA5/t2m_era5_
 
 # split CERRA data by hour
-cdo splithour -sellonlatbox,-6.86,5.11,44.96,36.99 -select,name=t2m ${projectroot}/dat/TRAINING/RAW/CERRA/t2m_cerra_[!lsm]*_005deg.nc ${projectroot}/dat/TRAINING/PREPROCESSED/CERRA/t2m_cerra_
+cdo splithour -sellonlatbox,-6.86,5.11,44.96,36.99 -select,name=t2m ${projectroot}/dat/TESTING/RAW/CERRA/t2m_cerra_[!lsm]*_005deg.nc ${projectroot}/dat/TESTING/PREPROCESSED/CERRA/t2m_cerra_
