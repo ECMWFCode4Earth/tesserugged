56afbc2fb46042a929bb4625345a82cb0b1c445b
Konrad Mayer <konrad.mayer@zamg.ac.at>
2023-07-20 11:03:26 +0000

    REF: parametrize dataset at regridding

--- dev/preprocessing/regrid_era5.sh
+++ dev/preprocessing/regrid_era5.sh
@@ -11,6 +11,7 @@
 # find project root (by finding git root) to be able to start the script from
 # anywhere within the project, but still avoid absolute paths
 projectroot=$(git rev-parse --show-toplevel)
+dataset=${1:-"TRAINING"}
 
 # Declare a string array with type
 declare -a leadtime=( "00" "03" "06" "09" "12" "15" "18" "21" )
@@ -21,5 +22,5 @@ for lt in "${leadtime[@]}"; do
     # create descriptor file for target grid
     stdbuf -oL cdo griddes dat/TRAINING/RESIDUALS/CERRA/t2m_cerra_${lt}_residuals.nc > /tmp/targetgrid.txt
     # bilinear interpolation
-    cdo remapbil,/tmp/targetgrid.txt ${projectroot}/dat/TRAINING/RESIDUALS/ERA5/t2m_era5_${lt}_residuals.nc ${projectroot}/dat/TRAINING/RESIDUALS/ERA5_regridded/t2m_era5_${lt}_residuals.nc
+    cdo remapbil,/tmp/targetgrid.txt ${projectroot}/dat/${dataset}/RESIDUALS/ERA5/t2m_era5_${lt}_residuals.nc ${projectroot}/dat/${dataset}/RESIDUALS/ERA5_regridded/t2m_era5_${lt}_residuals.nc
 done 
