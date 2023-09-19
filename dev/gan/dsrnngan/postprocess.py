ae2ced3016a429f1bb68ffe1ddc3a9b70c07d9ff
seblehner <sebastian.lehner@geosphere.at>
2023-09-19 16:35:41 +0000

    DEV: add small prediction postprocessing script

---
+++ dev/gan/dsrnngan/postprocess.py
@@ -0,0 +1,16 @@
+import xarray as xr
+
+
+dalist = []
+for year in [2019, 2020, 2021]:
+    dayear = xr.load_dataarray(
+        f"/data/TESTING/GAN/predictions/prediction_gan_ensemble_00_{year}.nc"
+    )
+    dalist.append(dayear)
+
+dafull = xr.concat(dalist, dim="time")
+outpath = "/data/TESTING/GAN/predictions/prediction_gan_ensemble_00.nc"
+dafull.to_netcdf(outpath)
+
+print(dafull)
+print(f"Saved data to {outpath}")
